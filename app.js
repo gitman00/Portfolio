94ddabf8911c6921ba66c3f613a935c7533e8385
gitman00 <ghimiremanish007@gmail.com>
2021-11-09 14:20:16 +0545

    updated files

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 // Declaring variables
 
 window.addEventListener("scroll", function () {
+
     let header = document.querySelector('header');
     header.classList.toggle('active', window.scrollY > 0);
+
+    let wrapper = document.querySelector('.wrapper');
+    wrapper.classList.toggle('active', window.scrollY > 0);
 })
 
 function show() {
     let menu = document.querySelector('.menu-icons');
     menu.classList.toggle('active');
+
+    let nav = document.querySelector('nav');
+    nav.classList.toggle('active');
 }
