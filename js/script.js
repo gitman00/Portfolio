f5be9054da49ad18d7a8717c6f9f00b28a25b4eb
gitman00 <ghimiremanish007@gmail.com>
2022-01-12 17:43:01 +0545

    Added files and assets

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/* ============================== typing animation ============================ */
+var typed = new Typed(".typing",{
+    strings:["","Web Designer","Web Developer","Graphic Designer","YouTuber"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+
+
+
+
+
+
+
+
